d99a01fb0fd724726f25db11ba627527d0195d28
brittanythomps <39777218+brittanythomps@users.noreply.github.com>
2018-05-31 10:05:03 -0600

    Update index.js

--- firstTry/index.js
+++ firstTry/index.js
@@ -18,17 +18,12 @@
 'use strict'
 
 const { TransactionProcessor } = require('sawtooth-sdk/processor')
-const IntegerKeyHandler = require('./firstTry_handler')
+const FirstTryHandler = require('./firstTry_handler')
 
-if (process.argv.length < 3) {
-  console.log('missing a validator address')
-  process.exit(1)
-}
+const transactionProcessor = new TransactionProcessor('tcp://localhost:4004')
 
-const address = process.argv[2]
-
-const transactionProcessor = new TransactionProcessor(address)
-
-transactionProcessor.addHandler(new IntegerKeyHandler())
+transactionProcessor.addHandler(new FirstTryHandler())
 
 transactionProcessor.start()
+
+console.log('Starting firsttry transaction processor')
